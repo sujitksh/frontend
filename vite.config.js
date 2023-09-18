6cbf3c266b93c1bdb35b0e6c0d047a61481142b4
skg <sujitkushwaha@gmail.com>
2023-09-18 21:27:43 +0530

    modify vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/api":"http://3.110.193.147/:8000" 
+      "/api":"http://3.110.193.147:8000" 
     },
     watch: {
       usePolling: true,
