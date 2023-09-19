5df53816db3234fcd256df88cbdc884ed52629e2
skg <sujitkushwaha@gmail.com>
2023-09-19 19:12:48 +0530

    change in vite file

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      "/api":"http://43.205.232.204:8000" 
-    },
-    watch: {
-      usePolling: true,
-    },
-    host: true, 
-    strictPort: true,
-    port: 3000,
-  },
   plugins: [react()],
 })
